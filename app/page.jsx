603842b92ddda2a8fd1ec048e5b9f8008724d173
Moses Echela <echelachris@gmail.com>
2023-07-20 19:07:43 +0100

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,37 @@
-import Feed from '@components/Feed';
+import Feed from "@components/Feed";
 import Image from "next/image";
-
 
 const home = () => {
   return (
-    <section className="w-full flex-center flex-col" 
-    style={{ backgroundImage: "url('/images/AI-bg.png')" }}
+    <section
+      className="w-full flex-center flex-col"
+      style={{
+        backgroundImage: "url('/images/AI-bg.png')", // Adjust the path to your background image
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
     >
-        <h1 className="head_text text-center">
-        Explore & Exchange  <br className="max-md:hidden"/> 
-            <span className="orange_gradient text-center">AI-Generated  Prompts</span>
-        
-        </h1>
-        <p className="desc text-center">
-            Promptpal is an open-source AI prompting tool for modern world to discover, create
-            and share creative prompts.
-        </p>
+      <h1 className="head_text text-center">
+        Explore & Exchange <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          AI-Generated Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Promptpal is a cutting-edge, open-source AI prompting tool designed to
+        ignite creativity and inspiration in the modern world. Discover, create,
+        and share an endless array of captivating prompts that elevate your
+        creative endeavors to new heights..
+      </p>
 
-    <Feed />
-        
+      <p className="text-center orange_gradient font-bold text-2xl mt-4 ">
+        "Unleash Your Imagination"
+      </p>
+
+      <Feed />
     </section>
-  )
-}
+  );
+};
 
 export default home;
